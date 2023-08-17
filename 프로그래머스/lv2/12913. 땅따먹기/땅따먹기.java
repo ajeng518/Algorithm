a62a72e6652a211f93cab799156d7dc8d300096b
ajeng518 <108220312+ajeng518@users.noreply.github.com>
2023-08-18 01:24:13 +0900

    [level 2] Title: 땅따먹기, Time: 19.83 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,22 @@
+class Solution {
+    static int idx, max,n, sum;
+    static int[][]temp;
+    
+    int solution(int[][] land) { 
+        int n=land.length;
+        
+        for(int i=1;i<n;i++){
+            land[i][0]+=Math.max(land[i-1][1], Math.max(land[i-1][2],land[i-1][3]));
+            land[i][1]+=Math.max(land[i-1][0], Math.max(land[i-1][2],land[i-1][3]));
+            land[i][2]+=Math.max(land[i-1][0], Math.max(land[i-1][1],land[i-1][3]));
+            land[i][3]+=Math.max(land[i-1][0], Math.max(land[i-1][1],land[i-1][2]));
+        }
+        
+        int max=Math.max(land[n-1][0],Math.max(land[n-1][1],Math.max(land[n-1][2],land[n-1][3])));
+        return max;
+        
+        
+        
+    }
+    
+}
