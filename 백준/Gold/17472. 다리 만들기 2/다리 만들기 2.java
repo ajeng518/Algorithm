d6d58153a29e8f6805592b41caa83435abf0634a
ajeng518 <108220312+ajeng518@users.noreply.github.com>
2023-10-10 21:11:36 +0900

    [Gold I] Title: 다리 만들기 2, Time: 80 ms, Memory: 11784 KB -BaekjoonHub

--- 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.java
+++ 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.java
@@ -3,43 +3,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Deque;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main {
-	static class MST_Node implements Comparable<MST_Node> {
-		int n1, n2;
-		int len;
-
-		public MST_Node(int n1, int n2, int len) {
-			super();
-			this.n1 = n1;
-			this.n2 = n2;
-			this.len = len;
-		}
-
-		@Override
-		public int compareTo(MST_Node o) {
-			return this.len - o.len;
-		}
-
-	}
-
-	static int N, M, map[][],parent[], idx;
+public class Main{
+	static int N, M, map[][], parent[], idx;
+	static List<int[]>[] list;
+	static boolean[][] chk;
 	static int[] dX = { -1, 0, 1, 0 };
 	static int[] dY = { 0, 1, 0, -1 };
-	static boolean chk[][];
-	static PriorityQueue<MST_Node> pq;
-	static List<int[]>[] list;
+	static PriorityQueue<int[]> pq;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
+
 		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());//
+		M = Integer.parseInt(st.nextToken());//
 		map = new int[N][M];
 
 		for (int i = 0; i < N; i++) {
@@ -49,80 +34,81 @@ public class Main {
 			}
 		}
 
+		list = new ArrayList[7];
+		chk=new boolean[N][M];
 		idx = 0;
-		list = new ArrayList[7];// 섬 최대 6개임
-		chk = new boolean[N][M];
-		
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				if (map[i][j] == 0)
-					continue;
-				if (chk[i][j])
-					continue;
-				list[++idx] = new ArrayList<>();
-				bfs(i, j, idx);
+				if (map[i][j] > 0 && !chk[i][j]) {
+					list[++idx] = new ArrayList();
+					bfs(i, j, idx);
+				}
 			}
 		}
+
+		pq = new PriorityQueue<>(new Comparator<int[]>() {
+
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o1[2] - o2[2];
+			}
+		});
 		
-		parent=new int[idx+1];
-		for(int i=1;i<=idx;i++) {
-			parent[i]=i;			
+		parent = new int[idx + 1];
+		for (int i = 1; i <= idx; i++) {
+			parent[i] = i;
 		}
-		
-		pq=new PriorityQueue<>();
-		
+
 		for (int i = 1; i <= idx; i++) {
 			for (int j = 0; j < list[i].size(); j++) {
-				int[] now = list[i].get(j);
-				for (int k = 0; k < 4; k++) {// 상하좌우
-					makeLine(now[0], now[1], i, k, -1);// x,y,현재섬 번호, 진행방향, count
+				int[]now=list[i].get(j);
+				for (int k = 0; k < 4; k++) {
+					makeBridge(now[0], now[1], i, k, -1);
 				}
 			}
 		}
 		
-		System.out.println(prim());
+		System.out.println(kruskal());
 
 	}
 
-	private static int prim() {
-		int result = 0;
+	private static int kruskal() {
 		int bridge=0;
+		int len=0;
 		
-		while(!pq.isEmpty()) {
-			MST_Node node=pq.poll();
-//			System.out.println(node.n1+", ");
-			int p1=find(node.n1);
-			int p2=find(node.n2);
-			
-//			System.out.println(p1+", "+p2);
-			
-			if(p1!=p2) {
-				union(p1,p2);
-				result+=node.len;
-//				System.out.println(node.len);
-				bridge++;	
+		
+		while (!pq.isEmpty()) {
+			int[] cur = pq.poll();
+			int p1 = find(cur[0]);
+			int p2 = find(cur[1]);
+
+			if (p1 != p2) {
+				union(p1, p2);
+				len+=cur[2];
+				bridge++;
 			}
 		}
-		if(result==0)return -1;
-		if(bridge!=idx-1)return -1;
 		
-		return result;
-
+		if(len<2)return -1;
+		if(bridge<idx-1)return -1;
+		return len;
 	}
 
 	private static void union(int p1, int p2) {
-		parent[p1]=p2;
+		parent[p1] = p2;
 	}
 
 	private static int find(int node) {
-		if(parent[node]==node)return parent[node];
-		else return parent[node]=find(parent[node]);
+		if (node == parent[node])
+			return parent[node];
+		return parent[node] = find(parent[node]);
 	}
 
-	private static void makeLine(int x, int y, int num, int way, int cnt) {
-		if (map[x][y] != num && map[x][y] != 0) {
+	private static void makeBridge(int x, int y, int idx, int way, int cnt) {
+
+		if (map[x][y] > 0 && map[x][y] != idx) {
 			if (cnt >= 2) {
-				pq.offer(new MST_Node(num, map[x][y], cnt));
+				pq.add(new int[] { idx, map[x][y], cnt });
 			}
 			return;
 		}
@@ -134,42 +120,46 @@ public class Main {
 			return;
 		if (nY < 0 || nY >= M)
 			return;
-		if (map[nX][nY] == num)
+		if (map[nX][nY] == idx)
 			return;
-		makeLine(nX, nY, num, way, cnt + 1);
+
+		makeBridge(nX, nY, idx, way, cnt + 1);
 
 	}
 
-	private static void bfs(int x, int y, int index) {
-		Deque<int[]> q = new ArrayDeque<>();
-
-		q.offer(new int[] { x, y });
+	// 섬마다 숫자 매핑하기(BFS)
+	private static void bfs(int x, int y, int idx) {
+		Deque<int[]> q = new ArrayDeque<int[]>();
+		q.add(new int[] { x, y });
 		chk[x][y] = true;
 
 		while (!q.isEmpty()) {
-			int[] now = q.poll();
-			map[now[0]][now[1]]=index;
-			list[index].add(new int[] { now[0], now[1] });
+			int[] cur = q.poll();
+			map[cur[0]][cur[1]] = idx;
+			list[idx].add(cur);
 
 			for (int i = 0; i < 4; i++) {
-				int nX = now[0] + dX[i];
-				int nY = now[1] + dY[i];
+				int nX = cur[0] + dX[i];
+				int nY = cur[1] + dY[i];
 
 				if (nX < 0 || nX >= N)
 					continue;
 				if (nY < 0 || nY >= M)
 					continue;
-
 				if (chk[nX][nY])
 					continue;
 				if (map[nX][nY] == 0)
 					continue;
 
-				q.offer(new int[] { nX, nY });
+				q.add(new int[] { nX, nY });
 				chk[nX][nY] = true;
 			}
-		}
 
+		}
 	}
+
+	// 섬에 다리 놓기(dfs)--> list에 다리 최소길이 정보 입력
+
+	// 최소 스패닝 트리 연결(크루스칼)
 
 }
