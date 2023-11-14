ca751033b9fde3f725e8e104b4bba903b8f4be03
ajeng518 <108220312+ajeng518@users.noreply.github.com>
2023-11-14 22:27:13 +0900

    [Unrated] Title: [모의 SW 역량테스트] 핀볼 게임, Time: 474 ms, Memory: 42,640 KB -BaekjoonHub

--- SWEA/Unrated/5650. ［모의 SW 역량테스트］ 핀볼 게임/［모의 SW 역량테스트］ 핀볼 게임.java
+++ SWEA/Unrated/5650. ［모의 SW 역량테스트］ 핀볼 게임/［모의 SW 역량테스트］ 핀볼 게임.java
@@ -5,32 +5,34 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class Solution{
-	static int[] dx = { -1, 0, 1, 0 };// 위-오-아래-왼
+class Solution {
+	static int N, M, map[][], max;
+	static int[] dx = { -1, 0, 1, 0 };// 상-우-하-좌
 	static int[] dy = { 0, 1, 0, -1 };
-	static int[][] type = { { 0, 0, 0, 0 }, { -1, -1, 1, 0 }, { 1, -1, -1, 2 }, { 3, 2, -1, -1 }, { -1, 0, 3, -1 },
+	static int[][] block = { {}, { -1, -1, 1, 0 }, { 1, -1, -1, 2 }, { 3, 2, -1, -1 }, { -1, 0, 3, -1 },
 			{ -1, -1, -1, -1 } };
 	static List<int[]>[] list;
-	static int N, map[][];
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine().trim());
+
+		int T = Integer.parseInt(br.readLine());
 
 		for (int test = 1; test <= T; test++) {
-			N = Integer.parseInt(br.readLine().trim());
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());// 세로
 			map = new int[N][N];
-			int max = Integer.MIN_VALUE;
-
+			max=Integer.MIN_VALUE;
+			
 			list = new ArrayList[11];
 			for (int i = 6; i < 11; i++) {
 				list[i] = new ArrayList<>();
 			}
 
 			for (int i = 0; i < N; i++) {
-				st = new StringTokenizer(br.readLine().trim());
+				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
 					if ((map[i][j] = Integer.parseInt(st.nextToken())) > 5) {
 						list[map[i][j]].add(new int[] { i, j });
@@ -42,18 +44,16 @@ public class Solution{
 				for (int j = 0; j < N; j++) {
 					if (map[i][j] != 0)
 						continue;
-					for (int k = 0; k < 4; k++) {
-						max = Math.max(max, goShooting(i, j, k));
-					}
+					for (int k = 0; k < 4; k++)
+						max = Math.max(max, goGame(i, j, k));
 				}
 			}
-
-			sb.append("#").append(test).append(" ").append(max).append('\n');
+			sb.append("#").append(test).append(" ").append(max).append("\n");
 		}
 		System.out.println(sb);
 	}
 
-	private static int goShooting(int x, int y, int way) {
+	private static int goGame(int x, int y, int way) {
 		int nx = x;
 		int ny = y;
 		int cnt = 0;
@@ -66,16 +66,11 @@ public class Solution{
 				cnt = cnt * 2 + 1;
 				break;
 			}
-			
-			if (nx == x && ny == y) {
+			if (map[nx][ny] == -1)
 				break;
-			} else if (map[nx][ny] == -1) {
+			if (nx == x && ny == y)
 				break;
-			}
-			
-			if(map[nx][ny]==0)continue;
-			
-			if (map[nx][ny] > 5) {// 웜홀
+			if (map[nx][ny] > 5) {
 				int idx = map[nx][ny];
 				if (list[idx].get(0)[0] == nx && list[idx].get(0)[1] == ny) {
 					nx = list[idx].get(1)[0];
@@ -84,18 +79,15 @@ public class Solution{
 					nx = list[idx].get(0)[0];
 					ny = list[idx].get(0)[1];
 				}
-			} else if (map[nx][ny] > 0) {// 블럭
+			} else if (map[nx][ny] > 0) {
 				int idx = map[nx][ny];
-				way = type[idx][way];
+				way = block[idx][way];
 				if (way == -1) {
 					cnt = cnt * 2 + 1;
 					break;
-				} else {
-					cnt++;
 				}
+				cnt++;
 			}
-
-			
 		}
 		return cnt;
 	}
