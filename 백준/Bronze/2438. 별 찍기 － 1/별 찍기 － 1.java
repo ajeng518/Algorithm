96e00595017051bacfd57ab4606d99766d15dad8
여아정 <108220312+ajeng518@users.noreply.github.com>
2023-12-10 23:43:17 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 164 ms, Memory: 12612 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(br.readLine());
+		
+		for(int i=0;i<n;i++) {
+			for(int j=n;j>=n-i;j--) {
+				System.out.print("*");
+			}System.out.println();
+		}
+	}
+}
