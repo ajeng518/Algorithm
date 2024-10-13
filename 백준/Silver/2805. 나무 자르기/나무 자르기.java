cc00ba8ec8c7efa565e25eeeaa58040f6038dbce
여아정 <108220312+ajeng518@users.noreply.github.com>
2024-10-14 01:27:46 +0900

    [Silver II] Title: 나무 자르기, Time: 424 ms, Memory: 167820 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,50 +1,59 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int map[], N, M, max;
+    static int n, m, max;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		map = new int[N];
-		max = Integer.MIN_VALUE;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			map[i] = Integer.parseInt(st.nextToken());
-			max = Math.max(max, map[i]);
-		}
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-		long result = biSearch();
-		System.out.println(result);
-	}
+        max = Integer.MIN_VALUE;
 
-	private static long biSearch() {
-		long low = 0;
-		long high = max;
-		long mid = (high + low) / 2;
+        int[] tree = new int[n];
+        st = new StringTokenizer(br.readLine());
 
-		while (low <= high) {
-			mid = (high + low) / 2;
-			long now = 0;
-			for (int i = 0; i < N; i++) {
-				long diff = map[i] - mid;
-				if (diff > 0)
-					now += diff;
-			}
+        for (int i = 0; i < n; i++) {
+            tree[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, tree[i]);
+        }
 
-			if (now < M)
-				high = mid - 1;
-			else if(now>=M)
-				low = mid + 1;
-		}
-		return high;
-	}
+        long ans = binarySearch(n, m, tree);
+        System.out.println(ans);
+    }
 
+    public static long binarySearch(int n, int m, int[] tree) {
+        long start = 0;
+        long end = max;
+
+        while (start <= end) {
+            long mid = (start + end) / 2;
+            long sum = chkTreeLong(mid, tree);
+
+            if (sum < m) end = mid - 1;
+            else {
+                start = mid + 1;
+            }
+        }
+
+        return end;
+    }
+
+    public static long chkTreeLong(long mid, int[] tree) {
+        long sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            long diff = tree[i] - mid;
+            if (diff <= 0) continue;
+
+            sum += diff;
+        }
+
+        return sum;
+    }
 }
