5856cf4705dac8ef4651b53555856dd4e60379c1
여아정 <108220312+ajeng518@users.noreply.github.com>
2024-10-16 22:27:01 +0900

    [Gold IV] Title: 부분합, Time: 184 ms, Memory: 22324 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,51 +1,35 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int s = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
 
-        int[] num = new int[n];
         st = new StringTokenizer(br.readLine());
-        int sum = 0;
-        int answer = 1_000_001;
-
         for (int i = 0; i < n; i++) {
-            if ((num[i] = Integer.parseInt(st.nextToken())) >= s) {
-                answer = 1;
-            }
-            sum += num[i];
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        if (answer == 1_000_001) {
-            if (sum < s) {
-                answer = 0;
-            } else {
-                int sub = 0, start = 0, end = 0;
+        int start = 0;
+        int end = 0;
+        int sum = 0;
+        int cnt = 0;
+        int min = Integer.MAX_VALUE;
 
-                while (true) {
-                    if (sub < s) {//구간합이 s보다 작을 경우 end 값을 증가시킨다
-                        if(end==n){
-                            break;
-                        }
-                        sub += num[end++];
-
-                    } else{//구간합이 s보다 크거나 같다면 start값을 그 다음 값으로 증가시킨다.
-                        if(answer>end-start){
-                            answer=end-start;
-                        }
-                        sub -= num[start++];
-                    }
-                }
-            }
+        while (true) {
+            if (sum >= s) {
+                min = Math.min(min, end - start);
+                sum -= arr[start++];
+            } else if (end == n) break;
+            else sum += arr[end++];
         }
-        System.out.println(answer);//정답 출력해주새우
+
+        if(min==Integer.MAX_VALUE) min=0;
+        System.out.println(min);
     }
 }
