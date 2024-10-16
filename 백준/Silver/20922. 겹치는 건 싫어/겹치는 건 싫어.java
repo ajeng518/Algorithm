f40077aa6761ed6d4a9558b6dd3343802ed2b4a2
여아정 <108220312+ajeng518@users.noreply.github.com>
2024-10-16 23:20:55 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 244 ms, Memory: 37056 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
@@ -1,40 +1,36 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
         int[] numCnt = new int[100001];
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int ans = 0;
-
-        st=new StringTokenizer(br.readLine(), " ");
-        int[] numbers = new int[n];
-
-        for(int i=0;i<n;i++){
-            numbers[i]=Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int start=0;
-        int end=0;
+        int s = 0;
+        int e = 0;
+        int cnt = 0;
+        int max = Integer.MIN_VALUE;
 
-        while (end<n){
-            while(end<n && numCnt[numbers[end]]+1<=m){
-                numCnt[numbers[end]]++;
-                end++;
+        while (e < n) {
+            while (e < n && numCnt[arr[e]] + 1 <= k) {
+                numCnt[arr[e]]++;
+                e++;
             }
-            ans=Math.max(ans,end-start);
-            numCnt[numbers[start]]--;
-            start++;
+
+            max = Math.max(max, e-s);
+            numCnt[arr[s]]--;
+            s++;
         }
-
-
-        System.out.println(ans);
-
+        System.out.println(max);
     }
 }
