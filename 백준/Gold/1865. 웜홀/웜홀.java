5535118f6cb125a9b5b8f9b4fa88e6e1e657f9b7
여아정 <108220312+ajeng518@users.noreply.github.com>
2024-10-25 00:22:47 +0900

    [Gold III] Title: 웜홀, Time: 368 ms, Memory: 20096 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.java
+++ 백준/Gold/1865. 웜홀/웜홀.java
@@ -1,10 +1,10 @@
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
+        import java.util.ArrayList;
+        import java.util.Arrays;
+        import java.util.List;
+        import java.util.StringTokenizer;
 
 public class Main {
     static final int INF = 5_000_000;
@@ -61,52 +61,58 @@ public class Main {
 
             String answer = "NO";
 
-            for (int i = 1; i <= n; i++) {
-                if (!BellmanFord(n, 2 * m + w, i)) {
-                    answer = "YES";
-                    break;
-                }
-            }
+//            for (int i = 1; i <= n; i++) {
+//                if (!BellmanFord(n, 2 * m + w, i)) {
+//                    answer = "YES";
+//                    break;
+//                }
+//            }
 
 //            if (!BellmanFord(n, 2 * m + w, 1)) {
 //                answer = "YES";
 //            }
 
-            sb.append(answer).append("\n");
+            sb.append(BellmanFord(n, 2 * m + w, 1)).append("\n");
 
         }
 
         System.out.println(sb);
     }
 
-    public static boolean BellmanFord(int n, int mw, int start) {
+    public static String BellmanFord(int n, int mw, int start) {
         int[] dist = new int[n + 1];
         Arrays.fill(dist, INF);
 
 //        dist[start] = 0;
-        boolean isChange;
+//        boolean isChange;
 
-        for (int i = 1; i <= n; i++) {
-            isChange = false;
+        for (int i = 1; i < n; i++) {
+//            isChange = false;
 
             for (int j = 0; j < mw; j++) {
                 Node cur = list.get(j);
 
                 // if (dist[cur.start] == INF) continue;
-                if (dist[cur.end] <= dist[cur.start] + cur.cost) continue;
+                int newCose = dist[cur.start] + cur.cost;
+                if (dist[cur.end] <= newCose) continue;
 
-                dist[cur.end] = dist[cur.start] + cur.cost;
-                isChange = true;
-
-                if (i == n){
-                    return false;
-                }
+                dist[cur.end] = newCose;
+//                isChange = true;
 
             }
 
-            if(!isChange) break;
+//            if (!isChange) break;
         }
 
-        return true;
+
+        for (int j = 0; j < mw; j++) {
+            Node cur = list.get(j);
+
+            // if (dist[cur.start] == INF) continue;
+            if (dist[cur.end] <= dist[cur.start] + cur.cost) continue;
+            return "YES";
+        }
+
+        return "NO";
     }
 }
