5ef14bc1b5ee73991096e2d9d2f662f7bd81e8a1
여아정 <108220312+ajeng518@users.noreply.github.com>
2024-10-26 20:36:52 +0900

    [Gold IV] Title: 동전 1, Time: 80 ms, Memory: 11572 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -5,26 +5,27 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
-         int n=Integer.parseInt(st.nextToken());
-         int k=Integer.parseInt(st.nextToken());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-         int[]dp=new int[k+1];
-         int[] num=new int[n];
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-         for(int i=0;i<n;i++){
-            num[i]=   Integer.parseInt(br.readLine());
-         }
+        int[] num = new int[n];
+        int[] dp = new int[k + 1];
+        dp[0] = 1;
 
-         dp[0]=1;
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(br.readLine());
+        }
 
-         for(int i=0;i<n;i++){
-             for(int j=1;j<=k;j++){
-                 if(j>=num[i])
-                     dp[j]=dp[j]+dp[j-num[i]];
-             }
-         }
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= k; j++) {
+                if (j - num[i] < 0) continue;
+
+                dp[j] = dp[j] + dp[j - num[i]];
+            }
+        }
 
         System.out.println(dp[k]);
 
