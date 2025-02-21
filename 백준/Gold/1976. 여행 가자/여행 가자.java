04ad01017642f88ce3b679f4cf67fd26a0f380bd
여아정 <108220312+ajeng518@users.noreply.github.com>
2025-02-21 23:24:57 +0900

    [Gold IV] Title: 여행 가자, Time: 108 ms, Memory: 15956 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-  static int[][] map;
+  static int[] parents;
   static int n, m;
   
   public static void main(String[] args) throws Exception {
@@ -12,48 +12,42 @@ public class Main {
     n=Integer.parseInt(br.readLine());
     m=Integer.parseInt(br.readLine());
     
-    map=new int[n+1][n+1];
+    parents=new int[n+1];
+    for(int i=1;i<=n; i++) parents[i]=i;
+    
     for(int i=1; i<=n; i++){
       st=new StringTokenizer(br.readLine());
-      
-      for(int j=1; j<=n; j++){
-        map[i][j]=Integer.parseInt(st.nextToken());
+      for(int j=1;j<=n;j++){
+        if(Integer.parseInt(st.nextToken())==0) continue;
+        int parentA=findParents(i);
+        int parentB=findParents(j);
+
+        if(parentA>parentB){
+          parents[parentA]=parentB;
+        }else parents[parentB]=parentA;
       }
     }
-    
-    int[] plan=new int[m];
+
     st=new StringTokenizer(br.readLine());
-    for(int i=0;i<m;i++) plan[i]=Integer.parseInt(st.nextToken());
+
+    int now=Integer.parseInt(st.nextToken());
+    int nowP=findParents(now);
     
-    for(int i=0;i<m-1;i++){
-      if(!bfs(plan[i], plan[i+1])){
+    for(int i=1; i<m; i++){
+      now=Integer.parseInt(st.nextToken());
+      
+      if(nowP!=findParents(now)){ 
         System.out.println("NO");
         System.exit(0);
       }
     }
+    
     System.out.println("YES");
     
   }
 
-  private static boolean bfs(int start, int end){
-    Deque<Integer> q = new ArrayDeque<>();
-    boolean[] chk=new boolean[n+1];
-    q.add(start);
-    chk[start]=true;
-
-    while(!q.isEmpty()){
-      int cur=q.poll();
-      if(cur==end) return true;
-      
-      for(int i=1;i<=n;i++){
-        if(map[cur][i]==0) continue;
-        if(chk[i]) continue;
-        
-        chk[i]=true;
-        q.add(i);
-      }
-    }
-
-    return false;
-  }
+  private static int findParents(int cur){
+    if(cur==parents[cur]) return cur;
+    return findParents(parents[cur]);
+  } 
 }
