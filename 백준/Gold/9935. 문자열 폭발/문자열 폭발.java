a158e399947b885b4b6c89a849dff47e73ddfed4
여아정 <108220312+ajeng518@users.noreply.github.com>
2025-02-28 18:07:56 +0900

    [Gold IV] Title: 문자열 폭발, Time: 308 ms, Memory: 117340 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -1,50 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String chk = br.readLine();
-        int chkSize = chk.length();
-        Stack<Character> stack = new Stack<>();
+    String str=br.readLine();
+    String bomb=br.readLine();
 
-        for (int i = 0; i < s.length(); i++) {
-            stack.push(s.charAt(i));
+    int strLen=str.length();
+    int bombLen=bomb.length();
 
-            if (chkSize <= stack.size()) {
-                boolean flag = true;
+    StringBuilder sb=new StringBuilder();
+    for(int i=0;i<strLen; i++){
+      char cur=str.charAt(i);
+      sb.append(cur);
 
-                for (int j = 0; j < chkSize; j++) {
-                    if (stack.get(stack.size() - chkSize + j) != chk.charAt(j)) {
-                        flag = false;
-                        break;
-                    }
-                }
-
-                if (flag) {
-                    for (int j = 0; j < chkSize; j++) stack.pop();
-                }
-            }
-        }
-
-
-        StringBuilder sb = new StringBuilder();
-        if (stack.isEmpty()) System.out.println("FRULA");
-        else {
-            while (!stack.isEmpty()) {
-                sb.append(stack.pop());
-            }
-
-            System.out.println(sb.reverse());
-        }
-
-
+      if(bombLen>sb.length()) continue;
+      
+      String newStr= sb.substring(sb.length()-bombLen).toString();
+      if(newStr.equals(bomb)){
+        sb.delete(sb.length()-bombLen, sb.length());
+      }
+      
     }
+    if(sb.length()==0) System.out.println("FRULA");
+    else System.out.println(sb);
+  }
 }
