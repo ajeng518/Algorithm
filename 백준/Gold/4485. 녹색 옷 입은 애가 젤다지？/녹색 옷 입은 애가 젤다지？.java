be34134f3ee6c8af987fef288b1a8a223e94bda5
여아정 <108220312+ajeng518@users.noreply.github.com>
2025-03-02 00:13:33 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 160 ms, Memory: 17548 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -4,6 +4,22 @@ import java.io.*;
 public class Main {
   static int[] dx={-1, 0 ,1 ,0};
   static int[] dy={0, 1, 0, -1};
+
+  static class Node implements Comparable<Node>{
+    int x;
+    int y;
+    int cost;
+
+    Node(int x, int y, int cost){
+      this.x=x;
+      this.y=y;
+      this.cost=cost;
+    }
+
+    public int compareTo(Node o){
+      return this.cost-o.cost;
+    }
+  }
   
   public static void main(String[] args) throws Exception {
     BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
@@ -27,22 +43,22 @@ public class Main {
       for(int i=0;i<n;i++) Arrays.fill(dp[i], Integer.MAX_VALUE);
       dp[0][0]=map[0][0];
       
-      PriorityQueue<int[]> pq=new PriorityQueue<>((a1, a2)-> a1[2]-a2[2]);
-      pq.add(new int[]{0, 0, dp[0][0]});
+      PriorityQueue<Node> pq=new PriorityQueue<>();
+      pq.add(new Node(0, 0, dp[0][0]));
 
       while(!pq.isEmpty()){
-        int[] cur=pq.poll();
-        if(cur[0]==n-1 && cur[1]==n-1) break;
+        Node cur=pq.poll();
+        if(cur.x==n-1 && cur.y==n-1) break;
 
         for(int i=0;i<4;i++){
-          int nx=cur[0]+dx[i];
+          int nx=cur.x+dx[i];
           if(nx<0 || nx>=n) continue;
-          int ny=cur[1]+dy[i];
+          int ny=cur.y+dy[i];
           if(ny<0 || ny>=n) continue;
 
-          if(dp[nx][ny]<= dp[cur[0]][cur[1]]+map[nx][ny]) continue;
-          dp[nx][ny]= dp[cur[0]][cur[1]]+map[nx][ny];
-          pq.add(new int[]{nx, ny, dp[nx][ny]});
+          if(dp[nx][ny]<= dp[cur.x][cur.y]+map[nx][ny]) continue;
+          dp[nx][ny]= dp[cur.x][cur.y]+map[nx][ny];
+          pq.add(new Node(nx, ny, dp[nx][ny]));
         }
       }
       
