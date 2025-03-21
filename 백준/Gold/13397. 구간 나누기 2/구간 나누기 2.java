fa2bd1afe9738386536054a4319f413346eed8c8
여아정 <108220312+ajeng518@users.noreply.github.com>
2025-03-21 23:37:41 +0900

    [Gold IV] Title: 구간 나누기 2, Time: 96 ms, Memory: 12624 KB -BaekjoonHub

--- 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.java
+++ 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.java
@@ -1,57 +1,51 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int MAX =Integer.MAX_VALUE;
-    static int MIN=Integer.MIN_VALUE;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        int right = Integer.MIN_VALUE;
-        int[] arr = new int[n];
-        int[] sub = new int[m];
-
+        int[] num = new int[n];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            right = Math.max(right, arr[i]);
-        }
+        for (int i = 0; i < n; i++) num[i] = Integer.parseInt(st.nextToken());
 
-
-        System.out.println(BSearch(0, right, m, arr));
+        System.out.println(binarySearch(n, m, num));
     }
 
-    static int BSearch(int left, int right, int m, int[] arr) {
-        int mid;
+    private static int binarySearch(int n, int m, int[] num) {
+        int left = 0;
+        int right = 10_000;
 
-        while (left < right) {
-            mid = (left + right) / 2;
-            if (chkGroup(mid, arr) <= m) {
-                right = mid;
-            } else left = mid + 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (chkGroupCnt(mid, num) <= m) {
+                right=mid-1;
+            }else left=mid+1;
         }
-        return right;
+
+        return left;
     }
 
-    static int chkGroup(int mid, int[] arr) {
-        int cnt =1;
-        int max =MIN;
-        int min=MAX;
+    private static int chkGroupCnt(int mid, int[] num) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
 
-        for(int i=0;i<arr.length;i++){
-            max = Math.max(max, arr[i]);
-            min = Math.min(min, arr[i]);
+        int cnt = 1;
+        for (int i = 0; i < num.length; i++) {
+            min = Math.min(min, num[i]);
+            max = Math.max(max, num[i]);
 
-            if(max- min>mid){
+            if (max - min > mid) {
                 cnt++;
-                max=MIN;
-                min=MAX;
+                min = Integer.MAX_VALUE;
+                max = Integer.MIN_VALUE;
                 i--;
             }
         }
