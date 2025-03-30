3dd48cd35e7204dab3b86599d0d4b0edfa7eca90
여아정 <108220312+ajeng518@users.noreply.github.com>
2025-03-30 17:47:16 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 528 ms, Memory: 149320 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -1,44 +1,44 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] input = new int[n];
+        StringTokenizer st;
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) input[i] = Integer.parseInt(st.nextToken());
-
+        int n = Integer.parseInt(br.readLine());    
+        int[] num=new int[n];
+        
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            num[i]=Integer.parseInt(st.nextToken());
+        }
+        
         List<Integer> list = new ArrayList<>();
-        list.add(input[0]);
+        list.add(num[0]);
 
-        for (int i = 1; i < n; i++) {
-            if (list.get(list.size() - 1) < input[i])
-                list.add(input[i]);
-
-            else {
-                int idx = binarySearch(list, input[i]);
-                list.set(idx, input[i]);
+        for(int i=1; i<n;i++){
+            if(list.get(list.size()-1) < num[i])
+                list.add(num[i]);
+            else{
+                int idx = binarySearch(list, num[i]);
+                list.set(idx, num[i]);
             }
         }
+
         System.out.println(list.size());
-    }
+  }
 
-    private static int binarySearch(List<Integer> list, int root) {
+    private static int binarySearch(List<Integer> list, int root){
         int left = 0;
-        int right = list.size() - 1;
-        int mid = -1;
+        int right = list.size()-1;
+        int mid=-1;
 
-        while (left <= right) {
-            mid = (left + right) / 2;
+        while(left<=right){
+            mid=(left+right)/2;
 
-            if (list.get(mid) < root) left = mid + 1;
-            else if (list.get(mid) >= root) right = mid - 1;
-
+            if(list.get(mid)<root) left=mid+1;
+            else right=mid-1;
         }
 
         return left;
