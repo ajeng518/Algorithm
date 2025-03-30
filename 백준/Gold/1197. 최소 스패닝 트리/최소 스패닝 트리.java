4fde8aa3ac40700aecbb5667fadcf4cb69e23ec1
여아정 <108220312+ajeng518@users.noreply.github.com>
2025-03-30 16:36:22 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 536 ms, Memory: 57224 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,97 +6,68 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
+    static class Edge implements Comparable<Edge> {
+        int node;
+        int cost;
 
-	static class Node implements Comparable<Node> {
-		int s;
-		int e;
-		int cost;
+        public Edge(int node, int cost) {
+            this.node = node;
+            this.cost = cost;
+        }
 
-		public Node() {
-			super();
-		}
+        @Override
+        public int compareTo(Edge o) {
+            return this.cost - o.cost;
+        }
 
-		public Node(int s, int e, int cost) {
-			super();
-			this.s = s;
-			this.e = e;
-			this.cost = cost;
-		}
+    }
 
-		@Override
-		public int compareTo(Node o) {
-			// TODO Auto-generated method stub
-			return Integer.compare(this.cost, o.cost);// 오름차순
-		}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	}
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        List<Edge>[] nodeList = new List[n + 1];
+        for(int i=1;i<=n;i++) nodeList[i]=new ArrayList<>();
 
-	static int V, E;
-	static int parent[], rank[];
-	static PriorityQueue<Node>pq;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int len = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+            nodeList[start].add(new Edge(end, len));
+            nodeList[end].add(new Edge(start, len));
+        }
 
-		st = new StringTokenizer(br.readLine());
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
+        System.out.println(prim(n, m, nodeList));
 
-		pq=new PriorityQueue<>();
-		parent = new int[V + 1];
-		rank = new int[V + 1];
+    }
 
-		for (int i = 0; i < E; i++) {
-			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
-			int cost = Integer.parseInt(st.nextToken());
-			pq.add(new Node(s, e, cost));
-		}
-		initial();
+    private static long prim(int n, int m, List<Edge>[] list){
+        PriorityQueue<Edge> pq=new PriorityQueue<>();
+        pq.add(new Edge(1, 0));
+        boolean[] visited=new boolean[n+1];
+        long all=0;
+        int cnt=0;
 
-		int sum=0;
-		int cnt=0;
-		while(cnt!=V-1) {
-			Node n=pq.poll();
-			if(union(n.s, n.e)) {
-				cnt++;
-				sum+=n.cost;
-			}
-		}System.out.println(sum);
-	}
+        while(!pq.isEmpty()){
+            if(cnt==n)break;
 
-	private static boolean union(int x, int y) {
-		x = find(x);
-		y = find(y);
+            Edge cur=pq.poll();
 
-		if (x == y)
-			return false;
+            if(visited[cur.node]) continue;
 
-		if (rank[x] < rank[y]) {
-			rank[y] += rank[x];
-			parent[x] = y;
-		} else {
-			rank[x] += rank[y];
-			parent[y] = x;
-		}
-		return true;
+            visited[cur.node]=true;
+            all+=cur.cost;
+            cnt++;
+            for(Edge next: list[cur.node]){
+                pq.add(next);
+            }
+        }
 
-	}
-
-	private static int find(int x) {
-		if (x == parent[x])
-			return parent[x];
-		else
-			return parent[x] = find(parent[x]);
-	}
-
-	private static void initial() {
-		for (int i = 1; i <= V; i++) {
-			parent[i] = i;
-			rank[i] = 1;
-		}
-	}
+        return all;
+    }
 
 }
