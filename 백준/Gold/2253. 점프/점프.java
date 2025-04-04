f03f35500e0b56a73a8ad7ea570babe1468c2b9b
여아정 <108220312+ajeng518@users.noreply.github.com>
2025-04-05 00:45:16 +0900

    [Gold IV] Title: 점프, Time: 140 ms, Memory: 18952 KB -BaekjoonHub

--- 백준/Gold/2253. 점프/점프.java
+++ 백준/Gold/2253. 점프/점프.java
@@ -1,63 +1,49 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine().trim());
 
-        int n = Integer.parseInt(st.nextToken().trim());
-        int m = Integer.parseInt(st.nextToken().trim());
+        int n =Integer.parseInt(st.nextToken().trim());
+        int m =Integer.parseInt(st.nextToken().trim());
 
-        List<Integer> cantGo = new ArrayList<>();
-        String input;
-        int cnt = 0;
-        while (cnt < m) {
-            if ((input = br.readLine().trim()).equals("")) continue;
+        Set<Integer> noStep = new HashSet<>();
+        while(m-->0) noStep.add(Integer.parseInt(br.readLine().trim()));
+        
+        int MAXINT=1000001;
+        int max = (int)Math.sqrt(n*2)+2;
+        
+        int[][] dp=new int[n+1][max];
+        for(int i=0;i<=n;i++) Arrays.fill(dp[i], MAXINT);
 
-            cantGo.add(Integer.parseInt(input));
-            cnt++;
-        }
+        dp[1][0]=0;
 
-        int maxJump = (int) Math.sqrt(2 * n) + 2;
-        int[][] dp = new int[n + 1][maxJump];
-
-        for (int i = 0; i <= n; i++) {
-            Arrays.fill(dp[i], 10001);
-        }
-
-        dp[1][0] = 0;
-
-        for (int i = 2; i <= n; i++) {
-            if (cantGo.contains(i)) continue;
-
-            int limit = (int) Math.sqrt(2 * i) + 1;
-
-            for (int v = 1; v <= limit; v++) {
-                if (i - v < 0) continue;
+        for(int i=2;i<=n;i++){
+            if(noStep.contains(i)) continue;
+            
+            int limit = (int)Math.sqrt(n*2)+1;
+            
+            for(int j = 1; j <= limit; j++){
+                if(i - j < 0) continue;
                 
-                if (v - 1 >= 0 && v + 1 < maxJump) { // 경계 조건을 확인합니다.
-                    dp[i][v] = Math.min(dp[i - v][v - 1], Math.min(dp[i - v][v], dp[i - v][v + 1])) + 1;
-                } else if (v - 1 >= 0) { // `v + 1`이 범위를 벗어날 경우
-                    dp[i][v] = Math.min(dp[i - v][v - 1], dp[i - v][v]) + 1;
-                } else { // `v - 1`도 범위를 벗어날 경우
-                    dp[i][v] = dp[i - v][v] + 1;
+                if(j-1>=0 && j+1<max){
+                    dp[i][j]=Math.min(dp[i-j][j-1], Math.min(dp[i-j][j], dp[i-j][j+1])) + 1;
+                }else if(j-1>=0){
+                    dp[i][j]=Math.min(dp[i-j][j-1], dp[i-j][j]) + 1;
+                }else{
+                    dp[i][j]=dp[i-j][j]+1;
                 }
             }
         }
 
-        int maxValue = 10001;
-
-        for (int i = 0; i < maxJump; i++) {
-            maxValue = Math.min(maxValue, dp[n][i]);
+        int minAns = MAXINT;
+        for(int i =0;i< max; i++){
+            minAns=Math.min(minAns, dp[n][i]);
         }
 
-        if (maxValue == 10001) maxValue = -1;
-
-        System.out.println(maxValue);
-    }
+        if(minAns >= MAXINT) System.out.println(-1);
+        else System.out.println(minAns);
+  }
 }
