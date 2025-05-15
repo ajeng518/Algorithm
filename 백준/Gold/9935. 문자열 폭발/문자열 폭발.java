e4ac740ec73c1d05db7ca6ea0f5de5767200a5b3
여아정 <108220312+ajeng518@users.noreply.github.com>
2025-05-15 16:14:23 +0900

    [Gold IV] Title: 문자열 폭발, Time: 284 ms, Memory: 117672 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -2,29 +2,27 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-  public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb=new StringBuilder();
 
-    String str=br.readLine();
-    String bomb=br.readLine();
+        char[] str=br.readLine().toCharArray();
+        String bomb=br.readLine();
+        int bombLen=bomb.length();
 
-    int strLen=str.length();
-    int bombLen=bomb.length();
+        for(int i=0;i<str.length; i++){
+            sb.append(str[i]);
 
-    StringBuilder sb=new StringBuilder();
-    for(int i=0;i<strLen; i++){
-      char cur=str.charAt(i);
-      sb.append(cur);
+            if(sb.length() < bombLen) continue;
 
-      if(bombLen>sb.length()) continue;
-      
-      String newStr= sb.substring(sb.length()-bombLen).toString();
-      if(newStr.equals(bomb)){
-        sb.delete(sb.length()-bombLen, sb.length());
-      }
-      
+            String tempStr=sb.substring(sb.length()-bombLen).toString();
+            if(tempStr.equals(bomb)){
+                sb.delete(sb.length()-bombLen, sb.length());
+            }
+        }
+
+        if(sb.length()==0) System.out.println("FRULA");
+        else System.out.println(sb);
+        
     }
-    if(sb.length()==0) System.out.println("FRULA");
-    else System.out.println(sb);
-  }
 }
