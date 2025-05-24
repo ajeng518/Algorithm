8c7d244158786d12ce7f58b54d8717d5701014e3
여아정 <108220312+ajeng518@users.noreply.github.com>
2025-05-25 02:55:04 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 60 ms, Memory: 11336 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -7,23 +7,22 @@ public class Main {
         StringBuilder sb=new StringBuilder();
         
         int t = Integer.parseInt(br.readLine());
+        int[][] apt = new int[15][15];
+        
+        for(int i = 1; i<=14;i++){
+            apt[0][i]=i;
+        }
+
+        for(int i =1; i<=14; i++){
+            for(int j =1; j<=14; j++){
+                apt[i][j]=apt[i][j-1]+apt[i-1][j];
+            }
+        }
         
         while(t-- >0 ){
             int k = Integer.parseInt(br.readLine());
-            int  n = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
 
-            int[][] apt = new int[k+1][n+1];
-            for(int i = 1;i<=n;i++){
-                apt[0][i]=i;
-            }
-
-            for(int i =1; i<=k; i++){
-                apt[i][1]=1;
-                
-                for(int j =2; j<=n; j++){
-                    apt[i][j]=apt[i][j-1]+apt[i-1][j];
-                }
-            }
 
             sb.append(apt[k][n]).append("\n");
         }
