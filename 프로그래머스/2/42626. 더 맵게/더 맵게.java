5ab19203f92f9e515bf7951913af9ebd2e033e4c
여아정 <108220312+ajeng518@users.noreply.github.com>
2025-06-11 23:45:40 +0900

    [level 2] Title: 더 맵게, Time: 1374.26 ms, Memory: 129 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        
+        for(int i=0;i<scoville.length; i++){
+            pq.add(scoville[i]);
+        }
+        
+        int cnt=0;
+        while(pq.peek()<K){
+            if(pq.size()==1) return -1;
+            
+            pq.add(pq.poll()+pq.poll()*2);
+            cnt++;
+        }
+        
+        return cnt;
+
+    }
+}
