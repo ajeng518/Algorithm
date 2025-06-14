ec18322a30778d229978dead09c54460120de049
여아정 <108220312+ajeng518@users.noreply.github.com>
2025-06-14 20:26:48 +0900

    [Silver III] Title: 계단 오르기, Time: 64 ms, Memory: 11624 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,40 +1,39 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(br.readLine());
-        int[] input = new int[n + 1];
-        for (int i = 1; i <= n; i++) input[i] = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int[] num=new int[n+1];
+        
+        for(int i=1;i<=n;i++){
+            num[i]=Integer.parseInt(br.readLine());
+        }
 
-        int[][] dp = new int[n + 1][2];
+        int[][] dp=new int[n+1][2];
+        dp[1][0]=num[1];
+        
+        if(n>1){
+            dp[2][0]=num[1]+num[2];
+            dp[2][1]=num[2];
 
-        dp[1][0] = input[1];
+            for(int i=3;i<=n;i++){
+                int temp=0;
 
-        if (n > 1) {
-            dp[2][0] = input[1] + input[2];
-            dp[2][1] = input[2];
+                dp[i][0]=dp[i-1][1]+num[i];
 
-            for (int i = 3; i <= n; i++) {
-                int jmp2 = 0;
+                if(dp[i-2][0] > dp[i-2][1]) temp = dp[i-2][0]+num[i];
+                else temp = dp[i-2][1]+num[i];
 
-                //1칸 점프
-                dp[i][0] = dp[i - 1][1] + input[i];
-
-                //2칸 점프
-                if (dp[i - 2][0] > dp[i - 2][1]) {
-                    jmp2 = dp[i - 2][0] + input[i];
-                } else {
-                    jmp2 = dp[i - 2][1] + input[i];
-                }
-
-                dp[i][1] = jmp2;
+                dp[i][1]=temp;
             }
         }
 
-        int max = dp[n][0] >= dp[n][1] ? dp[n][0] : dp[n][1];
+        int max = dp[n][0]>=dp[n][1]? dp[n][0]: dp[n][1];
+        
         System.out.println(max);
-    }
+  }
 }
