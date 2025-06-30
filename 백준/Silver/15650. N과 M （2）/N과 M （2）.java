0dec4b8c7dda57e935fe2d899b7db9d0ca9234f5
여아정 <108220312+ajeng518@users.noreply.github.com>
2025-07-01 00:38:40 +0900

    [Silver III] Title: N과 M (2), Time: 72 ms, Memory: 11572 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n, m;
+    static int[] arr;
+    static StringBuilder sb;
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        sb=new StringBuilder();
+
+        n = Integer.parseInt(st.nextToken());
+        m= Integer.parseInt(st.nextToken());
+        
+        arr=new int[m];
+        re(1, 0);
+
+        System.out.println(sb);
+    }
+    
+    private static void re(int now, int cnt){
+        if(cnt==m){
+            String curNum="";
+            
+            for(int i=0;i<m;i++){
+                curNum+=arr[i];
+                curNum+=" ";
+            }
+
+            sb.append(curNum).append("\n");
+            return;
+        }
+        if(now>n) return;
+
+        for(int i=now; i<=n;i++){
+            arr[cnt]=i;
+            re(i+1, cnt+1);
+        }
+    }
+}
