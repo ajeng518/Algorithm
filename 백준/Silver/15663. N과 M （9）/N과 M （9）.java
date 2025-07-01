cb96798ec8d7d300c02c67a77a485444f0d93c38
여아정 <108220312+ajeng518@users.noreply.github.com>
2025-07-01 18:47:23 +0900

    [Silver II] Title: N과 M (9), Time: 168 ms, Memory: 46904 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n, m;
+    static int[] num, arr;
+    static boolean[] visited;
+    static StringBuilder sb;
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        sb=new StringBuilder();
+
+        n = Integer.parseInt(st.nextToken());
+        m= Integer.parseInt(st.nextToken());
+        
+        num=new int[n];
+        arr=new int[m];
+
+        st=new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+         num[i]=Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(num);
+
+        visited=new boolean[n+1];
+        re(0);
+
+        System.out.println(sb);
+    }
+
+    private static void re(int cnt){
+        if(cnt==m){
+            String ansNum="";
+            for(int i=0;i<m;i++){
+                ansNum+=arr[i];
+                ansNum+=" ";
+            }
+
+            sb.append(ansNum).append("\n");
+            return;
+        }
+
+        int before=0;
+        for(int i=0;i<n;i++){
+            if(visited[i]) continue;
+            if(num[i]==before) continue;
+            
+            arr[cnt]=num[i];
+            
+            visited[i]=true;
+            before=num[i];
+            re(cnt+1);
+            visited[i]=false;
+        }
+    }
+}
