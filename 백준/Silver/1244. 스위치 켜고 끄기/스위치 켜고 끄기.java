990ad407a0f2cec844d2ee4dc27561019a3ee8e6
여아정 <108220312+ajeng518@users.noreply.github.com>
2025-07-02 17:08:47 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 68 ms, Memory: 11620 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,75 +1,75 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
+    static int n;
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb=new StringBuilder();
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
-		BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
-		
-		int n=Integer.parseInt(bf.readLine());//스위치 수
-		int[]swit=new int[n+1];
-		
-		StringTokenizer st=new StringTokenizer(bf.readLine()," ");//스위치 상태
-		for(int i=1;i<swit.length;i++) {
-			swit[i]=Integer.parseInt(st.nextToken());//매핑
-		}
-		
-		//남-1, 여-2
-		int person=Integer.parseInt(bf.readLine());//학생 수
-		int[][]stu=new int[person][2];
+        n= Integer.parseInt(br.readLine());
+        int[] switchList = new int[n+1];
 
-		for(int i=0;i<stu.length;i++) {
-			st=new StringTokenizer(bf.readLine()," ");
-			stu[i][0]=Integer.parseInt(st.nextToken());
-			stu[i][1]=Integer.parseInt(st.nextToken());
-		}
-		
-		for(int i=0;i<stu.length;i++) {
-			if(stu[i][0]==1) {//남
-				for(int j=1;j<swit.length;j++) {
-					if((j)%stu[i][1]==0) {
-						swit[j]=swit[j]>0?0:1;
-					}
-				}
-			}else {//여
-				int same=0;
-				if(stu[i][1]<=swit.length/2) {
-					for(int j=1;j<stu[i][1];j++) {
-						if(swit[stu[i][1]+j]==swit[stu[i][1]-j]) {
-							same++;
-						}else {break;}
-					}
-				}else {
-					for(int j=1;j<swit.length-stu[i][1];j++) {
-						if(swit[stu[i][1]+j]==swit[stu[i][1]-j]) {
-							same++;
-						}else {break;}
-					}
-				}
-				
-				if(same==0) {
-					swit[stu[i][1]]=swit[stu[i][1]]>0?0:1;
-				}else {
-					for(int j=1;j<=same;j++) {
-						swit[stu[i][1]+j]=swit[stu[i][1]+j]>0?0:1;
-//						System.out.println(stu[i][1]+j);
-						swit[stu[i][1]-j]=swit[stu[i][1]-j]>0?0:1;
-//						System.out.println(stu[i][1]+j);
-					}
-					swit[stu[i][1]]=swit[stu[i][1]]>0?0:1;
-				}
-			}
-		}
-		
-		for(int i=1;i<swit.length;i++) {
-			if(i>20 && i%20==1) {
-				System.out.println();
-			}
-			System.out.print(swit[i]+" ");
-		}
-	}
+        st=new StringTokenizer(br.readLine());
+        for(int i=1;i<=n;i++){
+            switchList[i]=Integer.parseInt(st.nextToken());
+        }
 
+        int student=Integer.parseInt(br.readLine());
+        while(student-- > 0){
+            st=new StringTokenizer(br.readLine());
+
+            int s = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
+
+            if(s==1){
+                boys(num,switchList);
+            }else{
+                girls(num,switchList);
+            }
+        }
+
+       for(int i=1;i<=n;i++){
+           sb.append(switchList[i]).append(" ");
+           if(i%20==0) sb.append("\n");
+       }
+
+        System.out.println(sb);
+        
+    }
+
+    private static void girls(int num, int[] switchList){
+        int left=num;
+        int right=num;
+        
+        while(left>0 && right<=n){
+            if(switchList[left]!=switchList[right]){
+                left++;
+                right--;
+                break;
+            }
+            left--;
+            right++;
+        }
+
+        if(left<1 || right>n){
+            left++;
+            right--;
+        }
+
+        for(int i=left; i<=right;i++){
+            switchList[i]=switchList[i]%2==0? 1: 0;
+        }
+    }
+
+    private static void boys(int num, int[] switchList){
+        int cur=num;
+
+        while(cur<=n){
+            switchList[cur]=switchList[cur]%2==0? 1 : 0;
+            cur+=num;
+        }
+    }
 }
