c4eff5b455285d06db207b90b4e25d6dfe12bb5f
여아정 <elegant518@gmail.com>
2025-09-23 16:59:44 +0900

    [Silver I] Title: 회전 초밥, Time: 116 ms, Memory: 16256 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -1,58 +1,65 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
+/*
+*리스트 배열 사용시 컴파일 경고 발생시 사용
+*@SuppressWarnings("unchecked")
+*/
+public class Main{
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-public class Main {
+        int n =Integer.parseInt(st.nextToken());
+        int d =Integer.parseInt(st.nextToken());
+        int k =Integer.parseInt(st.nextToken());
+        int c =Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st=new StringTokenizer(br.readLine());
-		
-		int n=Integer.parseInt(st.nextToken());
-		int d=Integer.parseInt(st.nextToken());
-		int k=Integer.parseInt(st.nextToken());
-		int c=Integer.parseInt(st.nextToken());
-		
-		int max=Integer.MIN_VALUE;
-		
-		Set<Integer>many;
-		List<Integer>list=new ArrayList<>();
-		ArrayDeque<Integer>q=new ArrayDeque<>();
-		
-		for(int i=0;i<n;i++) {
-			list.add(Integer.parseInt(br.readLine()));
-		}
-		
-		
-		for(int i=0;i<k;i++) {
-			q.add(list.get(i));
-		}
-		many=new HashSet<>(q);
-		many.add(c);
-		max=Math.max(max,many.size());
-		
-		int now=k;
-		for(int i=0;i<n;i++) {
-			if(now==list.size()) {
-				now=0;
-			}
-			q.poll();
-			q.add(list.get(now));
-			many=new HashSet<>(q);
-			many.add(c);
-//			System.out.println(many);
-			max=Math.max(max,many.size());
-			now++;
-		}
-		
-		System.out.println(max);
-	}
+        int[] sushi = new int[n];
+        for(int i=0;i<n;i++){
+            sushi[i]=Integer.parseInt(br.readLine());
+        }
 
+
+        int left = 0;
+        int right =0;
+        
+        int cnt=0;
+        int cateCnt=1;
+        
+        int[] visited=new int[d+1];
+        visited[c]=1;
+        
+        int max = 0;
+
+        while(left < n-1){
+            if(cnt == k){
+                visited[sushi[left]]--;
+                if(visited[sushi[left]] == 0){
+                    cateCnt--;
+                }
+                
+                left++;
+                if(right == n-1) right = -1;
+                
+                visited[sushi[++right]]++;
+                if(visited[sushi[right]] == 1) cateCnt++;
+
+            }else{
+                if(visited[sushi[right]] == 0){
+                    cateCnt++;
+                }
+                
+                visited[sushi[right]]++;
+                right++;
+                cnt++;
+                if(cnt ==k) right--;
+            }
+            
+            if(cnt == k){
+                max=Math.max(cateCnt, max);
+            }
+        }
+
+        System.out.println(max);
+    }
 }
