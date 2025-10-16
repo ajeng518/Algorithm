491b0794cc19f14429304be4868becdd667d8755
여아정 <elegant518@gmail.com>
2025-10-16 18:30:48 +0900

    Title: 계단 오르기, Time: 124ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 251016/계단 오르기/climbing-stairs.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+
+        int[] dp=new int[n+1];
+        if(n>=2)
+            dp[2]=1;
+        dp[3]=1;
+
+        for(int i =4;i<=n;i++){
+            dp[i]+=dp[i-2]+1;
+            dp[i]+=dp[i-3]+1;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
