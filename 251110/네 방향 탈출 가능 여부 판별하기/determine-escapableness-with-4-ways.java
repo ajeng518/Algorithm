5200cc1873c2f228ecd9a6eba0e64aacda9dfe03
여아정 <elegant518@gmail.com>
2025-11-10 22:59:34 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251110/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Main {
+    static int[] dx={-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] grid = new int[n][m];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                grid[i][j] = sc.nextInt();
+        // Please write your code here.
+    }
+
+    private static void bfs(int n, int m, int[][] grid){
+        boolean[][] visited = new boolean[n][m];
+        Deque<int[]> q= new ArrayDeque<>();
+
+        visited[0][0]=true;
+        q.add(new int[]{0, 0});
+
+        while(!q isEmpty()){
+            int[] cur = q.poll();
+
+            for(int i=0;i<4;i++){
+                int nx = cur[0]+dx[i];
+                if(nx < 0 || nx>= n) continue;
+                
+                int ny = cur[1]+dy[i];
+            }
+        }
+    }
+}
