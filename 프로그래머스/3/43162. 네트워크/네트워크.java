35dcbbfdc213c096f1f6cb090b20c1c847300c02
여아정 <elegant518@gmail.com>
2026-02-12 16:39:52 +0900

    [level 3] Title: 네트워크, Time: 10.00 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,41 +1,82 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
     static boolean[] visited;
     
     public int solution(int n, int[][] computers) {
-        int answer = 0;
+        int answer=0;
         visited=new boolean[n];
         
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n; i++){
             if(visited[i]) continue;
             
-            bfs(i, n, computers);
+            bfs(i, computers, n);
             answer++;
+            
         }
-        
         
         return answer;
     }
     
-    private static void bfs(int start, int n, int[][] computers){
+    private static void bfs(int start, int[][] computers, int n){
+        System.out.println(start+", ");
         
         Deque<Integer> q=new ArrayDeque<>();
         q.add(start);
-        visited[start] = true;
+        visited[start]=true;
         
         while(!q.isEmpty()){
             int cur = q.poll();
             
-            for(int j=0;j<n;j++){
-                if(visited[j]) continue;
-                if(computers[cur][j]==0) continue;
+            for(int i=0;i<n;i++){
+                if(visited[i]) continue;
+                if(computers[cur][i] == 0) continue;
                 
-                q.add(j);
-                visited[j]=true;
+                q.add(i);
+                visited[i]=true;
             }
         }
-        
-        return;
     }
 }
+
+
+// class Solution {
+//     static boolean[] visited;
+    
+//     public int solution(int n, int[][] computers) {
+//         int answer = 0;
+//         visited=new boolean[n];
+        
+//         for(int i=0;i<n;i++){
+//             if(visited[i]) continue;
+            
+//             bfs(i, n, computers);
+//             answer++;
+//         }
+        
+        
+//         return answer;
+//     }
+    
+//     private static void bfs(int start, int n, int[][] computers){
+        
+//         Deque<Integer> q=new ArrayDeque<>();
+//         q.add(start);
+//         visited[start] = true;
+        
+//         while(!q.isEmpty()){
+//             int cur = q.poll();
+            
+//             for(int j=0;j<n;j++){
+//                 if(visited[j]) continue;
+//                 if(computers[cur][j]==0) continue;
+                
+//                 q.add(j);
+//                 visited[j]=true;
+//             }
+//         }
+        
+//         return;
+//     }
+// }
