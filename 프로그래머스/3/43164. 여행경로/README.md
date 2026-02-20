c5e8b85bbb3af508f465d6ef982dc880c18f72c9
여아정 <elegant518@gmail.com>
2026-02-20 16:59:27 +0900

    [level 3] Title: 여행경로, Time: 129.62 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/README.md
+++ 프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164?language=java) 
 
 ### 성능 요약
 
-메모리: 110 MB, 시간: 82.30 ms
+메모리: 111 MB, 시간: 129.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 14일 22:54:20
+2026년 02월 20일 16:59:24
 
 ### 문제 설명
 
--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,33 +1,67 @@
 import java.util.*;
 
-class Solution {
-    int n;
-    boolean[] visited;
-    ArrayList<String> arr = new ArrayList<>();
-    
-    public void dfs(int cnt, String start, String path, String[][] tickets) {
-        if(cnt == n) {
-            arr.add(path);
-            return;
-        }
-        
-        for(int i = 0; i < n; i++) {
-            if (!visited[i] && tickets[i][0].equals(start)) {
-                visited[i] = true;
-                dfs(cnt+1, tickets[i][1], path + " " + tickets[i][1], tickets);
-                visited[i] = false;
-            }
-        }
-    }
+class Solution{
+    static List<String> road;
+    static boolean[] visited;
+    static int n;
     
     public String[] solution(String[][] tickets) {
+        road = new ArrayList<>();
         n = tickets.length;
         visited = new boolean[n+1];
         
         dfs(0, "ICN", "ICN", tickets);
         
-        Collections.sort(arr);
-
-        return arr.get(0).split(" ");
+        Collections.sort(road);
+        
+        return road.get(0).split(" ");        
+    }
+    
+    private static void dfs(int cnt, String start, String path, String[][]tickets ){
+        if(cnt==n){
+            road.add(path);
+            return;
+        }
+        
+        
+        for(int i =0; i< n; i++){
+            if(visited[i]) continue;
+            if(!tickets[i][0].equals(start)) continue;
+            visited[i]=true;
+            dfs(cnt+1, tickets[i][1], path+" "+tickets[i][1], tickets);
+            visited[i]=false;
+        }
     }
 }
+
+// class Solution {
+//     int n;
+//     boolean[] visited;
+//     ArrayList<String> arr = new ArrayList<>();
+    
+//     public void dfs(int cnt, String start, String path, String[][] tickets) {
+//         if(cnt == n) {
+//             arr.add(path);
+//             return;
+//         }
+        
+//         for(int i = 0; i < n; i++) {
+//             if (!visited[i] && tickets[i][0].equals(start)) {
+//                 visited[i] = true;
+//                 dfs(cnt+1, tickets[i][1], path + " " + tickets[i][1], tickets);
+//                 visited[i] = false;
+//             }
+//         }
+//     }
+    
+//     public String[] solution(String[][] tickets) {
+//         n = tickets.length;
+//         visited = new boolean[n+1];
+        
+//         dfs(0, "ICN", "ICN", tickets);
+        
+//         Collections.sort(arr);
+
+//         return arr.get(0).split(" ");
+//     }
+// }
