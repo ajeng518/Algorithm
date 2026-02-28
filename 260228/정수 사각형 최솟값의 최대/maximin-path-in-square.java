1f9ef279c721ded3db61ef47166357caad7c3a92
여아정 <elegant518@gmail.com>
2026-02-28 22:57:49 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 1644ms, Memory: 176MB, Status: Memory Limit Exceed - Codetree

--- 260228/정수 사각형 최솟값의 최대/maximin-path-in-square.java
+++ 260228/정수 사각형 최솟값의 최대/maximin-path-in-square.java
@@ -10,19 +10,38 @@ public class Main {
             }
         }
         // Please write your code here.
-        
+        int answer = bfs(n, matrix);
+        System.out.println(answer);
     }
     
-    private static int bfs(int[][] matrix){
-        Deque<int[]> q = new ArrayDeque<>();
-        q.add(new int[]{0, 0});
+    static int[] dx={0, 1};
+    static int[] dy={1, 0};
 
+    private static int bfs(int n, int[][] matrix){
+        Deque<int[]> q = new ArrayDeque<>();
+        q.add(new int[]{0, 0, matrix[0][0]});
         int max = 0;
 
         while(!q.isEmpty()){
-            while()
+
+            int[] cur = q.poll();
+
+            if(cur[0]==n-1 && cur[1]==n-1){
+                max = Math.max(max, cur[2]);
+                continue;
+            }
+
+            for(int i=0; i<2;i++){
+                int nx = cur[0]+dx[i];
+                if(nx < 0 || nx>= n) continue;
+
+                int ny = cur[1]+dy[i];
+                if(ny < 0 || ny>= n) continue;
+
+                q.add(new int[]{nx, ny, Math.min(cur[2], matrix[nx][ny])});
+            }
         }
 
-
+        return max;
     }
 }
