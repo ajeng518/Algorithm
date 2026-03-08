b94ddb35b91b508de80e8daa4ce7961214481e52
여아정 <elegant518@gmail.com>
2026-03-08 16:37:52 +0900

    Title: 최소 차 분할, Time: 136ms, Memory: 10MB, Status: Passed - Codetree

--- 260308/최소 차 분할/minimum-diff-partition.java
+++ 260308/최소 차 분할/minimum-diff-partition.java
@@ -12,20 +12,20 @@ public class Main {
         }
 
         int[][] dp=new int[n+1][sum+1];
-        // for(int i=0;i<=n;i++)
-        //     Arrays.fill(dp[i], -1);
+        for(int i=0;i<=n;i++)
+            Arrays.fill(dp[i], 101);
         dp[0][0]=0;
 
         for(int i=1;i<=n; i++){
             for(int j=1;j<=sum;j++){
                 if(j < arr[i] ) dp[i][j]=dp[i-1][j];
-                else dp[i][j]=Math.max(dp[i-1][j-arr[i]] + 1, dp[i-1][j]);
+                else dp[i][j]=Math.min(dp[i-1][j-arr[i]] + 1, dp[i-1][j]);
             }
         }
 
         int ans=Integer.MAX_VALUE;
         for(int i=0;i<=sum;i++){
-            if(dp[n][i]<=0) continue;
+            if(dp[n][i]>=101) continue;
             ans=Math.min(Math.abs(2*i -sum), ans);
         }
 
