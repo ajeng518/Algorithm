327ad909ce6b1394ce2dcae22da4829cb34df379
여아정 <elegant518@gmail.com>
2026-03-08 14:00:13 +0900

    Title: 배낭 채우기 2, Time: 152ms, Memory: 10MB, Status: Passed - Codetree

--- 260308/배낭 채우기 2/knapsack-2.java
+++ 260308/배낭 채우기 2/knapsack-2.java
@@ -5,13 +5,34 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        int[] weight = new int[n+1];
-        int[] value = new int[n+1];
+        int[] w = new int[n+1];
+        int[] v = new int[n+1];
         for (int i = 1; i <= n; i++) {
-            weight[i] = sc.nextInt();
-            value[i] = sc.nextInt();
+            w[i] = sc.nextInt();
+            v[i] = sc.nextInt();
         }
 
-        int[][] dp=new int[n+1][m+1];
+        int[] dp=new int[m+1];
+        // for(int i=0; i<=m; i++)
+        //     Arrays.fill(dp[i], -1);
+        Arrays.fill(dp, -1);
+        dp[0]=0;
+
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if(i < w[j]){
+                   continue;
+                }
+                dp[i]=Math.max(dp[i-w[j]] + v[j], dp[i]);
+            }
+        }
+
+        int ans=0;
+        for(int i=1;i<=m;i++){
+            ans=Math.max(dp[i], ans);
+        }
+
+        System.out.println(ans);
+
     }
 }
