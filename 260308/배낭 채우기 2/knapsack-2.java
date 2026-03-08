a0ff66bf28fe46ec3de1ec29f13267fb95dd54e4
여아정 <elegant518@gmail.com>
2026-03-08 13:33:50 +0900

    Title: 배낭 채우기 2, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260308/배낭 채우기 2/knapsack-2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] weight = new int[n+1];
+        int[] value = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+            weight[i] = sc.nextInt();
+            value[i] = sc.nextInt();
+        }
+
+        int[][] dp=new int[n+1][m+1];
+    }
+}
