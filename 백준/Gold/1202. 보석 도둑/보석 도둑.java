ff48f14e599d9181c6c6f0e858de61040c8fa0c6
여아정 <elegant518@gmail.com>
2026-03-29 17:05:23 +0900

    [Gold II] Title: 보석 도둑, Time: 1624 ms, Memory: 130596 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -1,67 +1,64 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
+/*
+*리스트 배열 사용시 컴파일 경고 발생시 사용
+*@SuppressWarnings("unchecked")
+*/
+public class Main{
+    static class Jewel implements Comparable<Jewel> {
+        int m;
+        int v;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Jewel(int m, int v){
+            this.m = m;
+            this.v=v;
+        }
+        
+        @Override
+        public int compareTo(Jewel o){
+            if(this.m == o.m)
+                return o.v - this.v;
+            return this.m-o.m;
+        }
+    }
+    public static void main(String args[]) throws Exception{
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-        int n, k;//보석 개수
-        long ans = 0;
-
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-
+        Jewel[] jewels = new Jewel[n];
         int[] bag = new int[k];
-        Jewel[] jewelList = new Jewel[n];
 
-        for (int i = 0; i < n; i++) {
+        for(int i=0; i<n; i++){
             st = new StringTokenizer(br.readLine());
-            jewelList[i] = new Jewel(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            jewels[i]=new Jewel(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
         }
-        Arrays.sort(jewelList);
-
-        for (int i = 0; i < k; i++) {
-            bag[i] = Integer.parseInt(br.readLine());
+        Arrays.sort(jewels);
+        
+        for(int i=0; i<k; i++){
+            bag[i]= Integer.parseInt(br.readLine());
         }
         Arrays.sort(bag);
 
-        for (int i = 0, j=0; i < k; i++) {
-            while(j<n){
-                if (jewelList[j].m > bag[i])
-                    break;
-                pq.offer(jewelList[j++].v);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        int j =0;
+        long ans=0;
+        
+        for(int i=0; i<k; i++){
+            while(j < n){
+                if(jewels[j].m > bag[i]) break;
+                
+                pq.add(jewels[j++].v);
             }
-            if(!pq.isEmpty()) {
+
+            if(!pq.isEmpty()){
                 ans += pq.poll();
             }
         }
+
         System.out.println(ans);
-
-    }
-
-    static class Jewel implements Comparable<Jewel> {
-        int m; //무게
-        int v;//가치
-
-        Jewel() {
-        }
-
-        Jewel(int m, int v) {
-            this.m = m;
-            this.v = v;
-        }
-
-        @Override
-        public int compareTo(Jewel o) {
-            if (this.m == o.m) {
-                return o.v - this.v;
-            }
-            return this.m - o.m;
-
-        }
     }
 }
