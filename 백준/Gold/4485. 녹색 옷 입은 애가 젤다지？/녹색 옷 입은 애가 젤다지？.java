74186aebbd06875f520e200dbd0eddd6478f99e8
여아정 <elegant518@gmail.com>
2026-03-29 15:43:43 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 276 ms, Memory: 22984 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,71 +1,66 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-  static int[] dx={-1, 0 ,1 ,0};
-  static int[] dy={0, 1, 0, -1};
+public class Main{
+    static int[][] map;
+    static int n;
+    
+    static int[] dx={-1, 0, 1, 0};
+    static int[] dy={0, 1, 0, -1};
+    
+    public static void main(String args[]) throws Exception{
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb=new StringBuilder();
 
-  static class Node implements Comparable<Node>{
-    int x;
-    int y;
-    int cost;
+        String input;
+        int idx=1;
 
-    Node(int x, int y, int cost){
-      this.x=x;
-      this.y=y;
-      this.cost=cost;
-    }
+        while(!(input=br.readLine()).equals("0")){
+            n = Integer.parseInt(input);
+            map = new int[n][n];
+            
+            for(int i=0;i<n;i++){
+                st= new StringTokenizer(br.readLine());
+                
+                for(int j=0;j<n;j++){
+                    map[i][j]=Integer.parseInt(st.nextToken());
+                }
+            }
 
-    public int compareTo(Node o){
-      return this.cost-o.cost;
-    }
-  }
-  
-  public static void main(String[] args) throws Exception {
-    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st;
-    StringBuilder sb=new StringBuilder();
-    int idx=1;
-
-    while(true){
-      int n=Integer.parseInt(br.readLine());
-      if(n==0) break;
-  
-      int[][] map=new int[n][n];
-      for(int i=0;i<n;i++){
-        st=new StringTokenizer(br.readLine());
-        for(int j=0;j<n;j++){
-          map[i][j]=Integer.parseInt(st.nextToken());
+            sb.append("Problem ").append(idx++).append(": ").append(dijkstra()).append("\n");
         }
-      }
-  
-      int[][] dp=new int[n][n];
-      for(int i=0;i<n;i++) Arrays.fill(dp[i], Integer.MAX_VALUE);
-      dp[0][0]=map[0][0];
-      
-      PriorityQueue<Node> pq=new PriorityQueue<>();
-      pq.add(new Node(0, 0, dp[0][0]));
-
-      while(!pq.isEmpty()){
-        Node cur=pq.poll();
-        if(cur.x==n-1 && cur.y==n-1) break;
-
-        for(int i=0;i<4;i++){
-          int nx=cur.x+dx[i];
-          if(nx<0 || nx>=n) continue;
-          int ny=cur.y+dy[i];
-          if(ny<0 || ny>=n) continue;
-
-          if(dp[nx][ny]<= dp[cur.x][cur.y]+map[nx][ny]) continue;
-          dp[nx][ny]= dp[cur.x][cur.y]+map[nx][ny];
-          pq.add(new Node(nx, ny, dp[nx][ny]));
-        }
-      }
-      
-      sb.append("Problem ").append(idx).append(": ").append(dp[n-1][n-1]).append("\n");
-      idx++;
+        
+        System.out.println(sb);
     }
 
-    System.out.println(sb);
-  }
+    private static int dijkstra(){
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a1, a2) -> a1[2]-a2[2]);
+        pq.add( new int[]{0, 0, map[0][0]});
+        int[][] dp = new int[n][n];
+        for(int i=0;i<n;i++) Arrays.fill(dp[i], 10*125*125);
+        dp[0][0] =map[0][0];
+
+        while(!pq.isEmpty()){
+            int[] cur = pq.poll();
+
+            if(cur[0]==n-1 && cur[1]==n-1){
+                return cur[2];
+            }
+
+            for(int i=0;i<4;i++){
+                int nx = cur[0]+dx[i];
+                if(nx < 0 || nx>=n) continue;
+                int ny = cur[1]+dy[i];
+                if(ny < 0 || ny>= n) continue;
+
+                if(dp[nx][ny] <=  cur[2] + map[nx][ny]) continue;
+                dp[nx][ny]= cur[2] + map[nx][ny];
+
+                pq.add(new int[]{nx, ny, dp[nx][ny]});
+            }
+        }
+
+        return dp[n-1][n-1];
+    }
 }
