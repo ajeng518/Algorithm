e6b8d17f63fb2530a6f4d3c09b75975875c1849d
여아정 <elegant518@gmail.com>
2026-03-30 17:14:19 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 2128 ms, Memory: 411628 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
@@ -1,49 +1,51 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-    static int[][] dp;
+public class Main{
+    static List<Integer>[] list;
+    static int n;
     static boolean[] visited;
-    static List<Integer>[] tree;
+    static int[][] dp;
     
-    public static void main(String[] args) throws Exception {
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String args[]) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int n =Integer.parseInt(br.readLine());
-        
-        dp=new int[n+1][2];
-        visited=new boolean[n+1];
-        tree=new List[n+1];
-        
-        for(int i=0;i<=n;i++)tree[i]=new ArrayList<>();
-        
-        while(--n > 0){
+        int n = Integer.parseInt(br.readLine());
+        list=new List[n+1];
+        for(int i=0;i<=n;i++) list[i]=new ArrayList<>();
+
+        for(int i=1; i<n;i++){
             st=new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
-            tree[a].add(b);
-            tree[b].add(a);
+            list[a].add(b);
+            list[b].add(a);
         }
-        dfs(1);
 
+        
+        visited = new boolean[n+1];
+
+        dp=new int[n+1][2];
+        
+        dfs(1);
+        
         System.out.println(Math.min(dp[1][0], dp[1][1]));
     }
 
-    private static void dfs(int num){
-        visited[num]=true;
+    private static void dfs(int cur){
+        visited[cur]=true;
 
-        dp[num][1]=1;
-        dp[num][0]=0;
-
-        for(int next : tree[num]){
+        dp[cur][1]=1;
+        dp[cur][0]=0;
+        
+        for(int next: list[cur]){
             if(visited[next]) continue;
 
             dfs(next);
             
-            dp[num][0]+=dp[next][1];
-            dp[num][1]+=Math.min(dp[next][1], dp[next][0]);
+            dp[cur][0] += dp[next][1];
+            dp[cur][1] += Math.min(dp[next][0], dp[next][1]);
         }
     }
 }
