1d694f3fd011ad9db329b257241d6ef7c4b6e00b
여아정 <elegant518@gmail.com>
2026-04-09 17:55:37 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,11 +1,25 @@
-SELECT  A.ID,
-        A.NAME,
-        A.HOST_ID
-FROM PLACES A JOIN (SELECT * 
-                    FROM PLACES B
-                    GROUP BY B.HOST_ID
-                    HAVING COUNT(B.HOST_ID) > 1
-                    ) C
-    ON A.HOST_ID = C.HOST_ID
-ORDER BY A.ID
+SELECT ID,
+        NAME,
+        HOST_ID
+FROM PLACES
+WHERE HOST_ID IN (
+            SELECT HOST_ID
+            FROM PLACES
+            GROUP BY HOST_ID
+            HAVING COUNT(HOST_ID)>1
+            )
+ORDER BY 1
 ;
+
+
+# SELECT  A.ID,
+#         A.NAME,
+#         A.HOST_ID
+# FROM PLACES A JOIN (SELECT * 
+#                     FROM PLACES B
+#                     GROUP BY B.HOST_ID
+#                     HAVING COUNT(B.HOST_ID) > 1
+#                     ) C
+#     ON A.HOST_ID = C.HOST_ID
+# ORDER BY A.ID
+# ;
