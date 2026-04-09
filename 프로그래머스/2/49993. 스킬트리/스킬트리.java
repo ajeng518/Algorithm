a8426939c9401a2d17e91a06da68093dabe09ff0
여아정 <elegant518@gmail.com>
2026-04-09 16:02:48 +0900

    [level 2] Title: 스킬트리, Time: 0.31 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,28 +1,55 @@
 import java.util.*;
-
-class Solution {
-    public int solution(String skill, String[] skill_trees) {
-        int answer = 0;
+class Solution{
+    public int solution(String skill, String[] skill_trees){
+        int ans = 0;
         
-        for(int i=0;i<skill_trees.length; i++){
-            char[] cur=skill_trees[i].toCharArray();
+        for(int i=0;i<skill_trees.length;i++){
+            char[] cur = skill_trees[i].toCharArray();
             
-            if(chkSkillPossible(skill, cur)) answer++;
-           
+            if(!chkSkills(skill, cur)) continue;
+            
+            ans++;
         }
-        return answer;
+        
+        return ans;
     }
     
-    private static boolean chkSkillPossible(String skill, char[] cur){
-        StringBuilder sb=new StringBuilder();
+    private static boolean chkSkills(String skill, char[] cur){
+        StringBuilder sb = new StringBuilder();
         
-        for(int i=0;i<cur.length; i++){
-            if(skill.contains(String.valueOf(cur[i]))){
+        for(int i=0; i<cur.length; i++){
+            if(skill.contains(String.valueOf(cur[i])))
                 sb.append(cur[i]);
-            }
         }
         
-        if(skill.indexOf(sb.toString())==0) return true;
-        else return false;
+        if(skill.indexOf(sb.toString()) == 0) return true;
+        return false;
     }
 }
+
+// class Solution {
+//     public int solution(String skill, String[] skill_trees) {
+//         int answer = 0;
+        
+//         for(int i=0;i<skill_trees.length; i++){
+//             char[] cur=skill_trees[i].toCharArray();
+            
+//             if(chkSkillPossible(skill, cur)) answer++;
+           
+//         }
+//         return answer;
+//     }
+    
+//     private static boolean chkSkillPossible(String skill, char[] cur){
+//         StringBuilder sb=new StringBuilder();
+        
+//         for(int i=0;i<cur.length; i++){
+//             if(skill.contains(String.valueOf(cur[i]))){
+//                 sb.append(cur[i]);
+//             }
+//         }
+        
+//         if(skill.indexOf(sb.toString())==0) return true;
+//         else return false;
+//     }
+// }
