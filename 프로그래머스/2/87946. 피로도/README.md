131f46e0e4534ff780d1373b8c380428156daf3b
여아정 <elegant518@gmail.com>
2026-04-09 15:02:12 +0900

    [level 2] Title: 피로도, Time: 5.46 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=java) 
 
 ### 성능 요약
 
-메모리: 87.3 MB, 시간: 3.55 ms
+메모리: 80.3 MB, 시간: 5.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 27일 23:28:27
+2026년 04월 09일 15:02:09
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,31 +1,65 @@
-class Solution {
-    static boolean[][] visited;
-    static int n, max;
+import java.io.*;
+import java.util.*;
+
+class Solution{
+    static boolean[] visited;
+    static int size, max;
     
-    public int solution(int k, int[][] dungeons) {//k:현재 피로도, dungeons:던전당 최소필요피로도, 소모피로도
-        int answer = -1;
-        n=dungeons.length;//던전 수
-        max=-1;
+    public int solution(int k , int[][] dungeons){
+        int answer=0;
+        size = dungeons.length;
+        max=0;
+        visited=new boolean[size];
         
-        re(k, 0, 0, dungeons, new boolean[n]);
-        
+        dfs(0, k, dungeons);
         return max;
     }
-    
-    private static void re(int now, int curDungeon, int cnt, int[][]dungeons, boolean[] visited){
-        if(now<=0) return;
-        max=Math.max(cnt, max);
-        
-        
-        for(int i=0; i<n; i++){
-            if(visited[i]) continue;
 
-            if(dungeons[i][0]>now)continue;
+    private static void dfs(int cnt, int nowHp, int[][] dungeons){
+        if(nowHp<=0){
+            return;
+        }
+        max=Math.max(max, cnt);
+        
+        for(int i=0;i<size;i++){
+            if(visited[i]) continue;
+            if(dungeons[i][0] > nowHp) continue;
             
             visited[i]=true;
-            re(now-dungeons[i][1], i, cnt+1, dungeons, visited);
+            dfs(cnt+1, nowHp-dungeons[i][1], dungeons);
             visited[i]=false;
         }
-        
     }
 }
+
+// class Solution {
+//     static boolean[][] visited;
+//     static int n, max;
+    
+//     public int solution(int k, int[][] dungeons) {//k:현재 피로도, dungeons:던전당 최소필요피로도, 소모피로도
+//         int answer = -1;
+//         n=dungeons.length;//던전 수
+//         max=-1;
+        
+//         re(k, 0, 0, dungeons, new boolean[n]);
+        
+//         return max;
+//     }
+    
+//     private static void re(int now, int curDungeon, int cnt, int[][]dungeons, boolean[] visited){
+//         if(now<=0) return;
+//         max=Math.max(cnt, max);
+        
+        
+//         for(int i=0; i<n; i++){
+//             if(visited[i]) continue;
+
+//             if(dungeons[i][0]>now)continue;
+            
+//             visited[i]=true;
+//             re(now-dungeons[i][1], i, cnt+1, dungeons, visited);
+//             visited[i]=false;
+//         }
+        
+//     }
+// }//
